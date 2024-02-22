6f9f26a83ceb820cf49d30626063d918f235faac
HeathCHEN <Heath.CHEN@foxmail.com>
2024-02-23 00:51:33 +0800

    feat: initProject

--- src/main/java/cn/heath/mybatisplus/util/strategy/SqlQueryTypeStrategy.java
+++ src/main/java/cn/heath/mybatisplus/util/strategy/SqlQueryTypeStrategy.java
@@ -3,6 +3,7 @@ package cn.heath.mybatisplus.util.strategy;
 import cn.heath.mybatisplus.util.annotation.CustomerQuery;
 import cn.heath.mybatisplus.util.enums.QueryType;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 
 import java.util.List;
@@ -20,7 +21,7 @@ public class SqlQueryTypeStrategy implements QueryTypeStrategy {
     @Override
     public <T> void buildQuery(CustomerQuery customerQuery, QueryWrapper<T> queryWrapper, Map<String, Object> objectMap, Object value, String[] orColumns, String underlineCase, List<String> usedProperties) {
 
-        if (ObjectUtil.isNull(value)) {
+        if (ObjectUtil.isNull(value)|| StrUtil.isBlank(customerQuery.sql())) {
             return;
         }
 
